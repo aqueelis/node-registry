e192415929960eb7a6abfbb19dd232478becbd94
aqueelis <aqueel.kmit10@gmail.com>
2026-01-17 17:52:24 -0500

    Updating app to directly read the Node from Task in ECS

--- app/app.py
+++ app/app.py
@@ -7,13 +7,25 @@ import hashlib
 app = Flask(__name__)
 
 APP_VERSION = os.getenv("APP_VERSION", "v1.0")
+ECS_METADATA_URL = os.getenv("ECS_CONTAINER_METADATA_URI_V4")
 
 def get_node_id():
-    hostname = socket.gethostname()
-    # Create a stable numeric suffix from hostname
-    hash_value = int(hashlib.md5(hostname.encode()).hexdigest(), 16)
-    suffix = hash_value % 100  # Node-00 to Node-99
-    return f"Node-{suffix:02d}", hostname
+    if not ECS_METADATA_URL:
+        return f"Node-{socket.gethostname()}"
+
+    try:
+        resp = requests.get(f"{ECS_METADATA_URL}/task", timeout=5)
+        resp.raise_for_status
+        #hostname = socket.gethostname()
+        # Create a stable numeric suffix from hostname
+        #hash_value = int(hashlib.md5(hostname.encode()).hexdigest(), 16)
+        #suffix = hash_value % 100  # Node-00 to Node-99
+        task_arn = resp.json().get("TaskARN", "unknown")
+        short_id = task_arn.split("/")[-1][:6]
+        return f"Node-{short_id}"
+    except Exception as e:
+        return "Node-Unknown"
+    
 
 @app.route("/")
 def index():
@@ -22,11 +34,11 @@ def index():
 
 @app.route("/api/info")
 def info():
-    node_id, hostname = get_node_id()
+    #node_id, hostname = get_node_id()
     return jsonify({
-        "node": node_id,
+        "node": socket.gethostname(),
         "version": APP_VERSION,
-        "hostname": hostname
+        "hostname": socket.gethostname()
     })
 
 @app.route("/health")
